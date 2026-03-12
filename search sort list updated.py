b73f256867c5912907d957b1b0b07ed53f7344b4
YASWANTH <yaswanthvanacharla@gmail.com>
2026-03-12 21:02:43 +0530

    Add search method to Solution class
    Implement search method to find target index in list.

---
+++ search sort list updated.py
@@ -0,0 +1,8 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if target in nums:
+            for i in range(0,len(nums)):
+                if target==nums[i]:
+                    return i
+        else:
+            return -1
