94fd428bc0cc008c6c436899384d93cdcfe647bb
YASWANTH <yaswanthvanacharla@gmail.com>
2026-07-21 09:11:58 +0530

    Remove redundant calculation in numberOfSteps method
    Removed unnecessary line calculating num%10.

--- 1342. Number of Steps to Reduce a Number to Zero.py
+++ 1342. Number of Steps to Reduce a Number to Zero.py
@@ -2,7 +2,6 @@ class Solution:
     def numberOfSteps(self, num: int) -> int:
         step=0
         while num>0:
-            num%10
             if num%2==0:
                 num=num/2
                 step+=1
