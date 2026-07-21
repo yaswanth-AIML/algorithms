8d9fde29f529e004326f661b5ad025108fc79648
YASWANTH <yaswanthvanacharla@gmail.com>
2026-07-21 09:09:59 +0530

    Implement numberOfSteps method to reduce number to zero

---
+++ 1342. Number of Steps to Reduce a Number to Zero.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numberOfSteps(self, num: int) -> int:
+        step=0
+        while num>0:
+            num%10
+            if num%2==0:
+                num=num/2
+                step+=1
+            else:
+                num-=1
+                step+=1
+        return step
+            
