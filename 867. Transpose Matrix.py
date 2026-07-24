a67d156c942aeefe6e90c819cbdb46271373714d
YASWANTH <yaswanthvanacharla@gmail.com>
2026-07-24 21:47:36 +0530

    Implement matrix transposition in Solution class

---
+++ 867. Transpose Matrix.py
@@ -0,0 +1,12 @@
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        leng=len(matrix)
+        row=len(matrix[0])
+        rel=[]
+        for i in range(row):
+            row1=[]
+            for j in range(leng):
+                row1.append(matrix[j][i]) 
+            rel.append(row1)
+        return rel
+                
